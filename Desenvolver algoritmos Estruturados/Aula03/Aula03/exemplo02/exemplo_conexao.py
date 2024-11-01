001e341c5ff9db209e43eba7f813dcfde0122028
EduardoAndradeEJ6 <contato.eduardo.ti@gmail.com>
2024-10-31 21:22:17 -0300

    Aula 4

--- Desenvolver algoritmos Estruturados/Aula03/Aula03/exemplo02/exemplo_conexao.py
+++ Desenvolver algoritmos Estruturados/Aula03/Aula03/exemplo02/exemplo_conexao.py
@@ -9,16 +9,19 @@ database = 'bd_produtos'
 def conecta_banco():
     try:
         # URL de conexão com o banco, usando o SQLALCHEMY E PYMYSQL
-        engine = create_engine(f'mysql+pymysql://{root}:{root}@{localhost}/bd_produtos')
+        engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}/{database}')
 
         # estabelece a conexão
         with engine.connect() as conexao:
-            query = "SELECT * FROM bd_produtos"
+            query = "SELECT * FROM vendas"
         
         #text(query) transforma a string da query em um objeto comaptivel com SQLALchemy
         #'conexao.consulte' executa essa consulta no banco de dados.
 
             resultados = conexao.execute(text(query))
+
+            for item in resultados:
+                print(f'Id: {item[0]}, Preço: {item[4]}, Quantidade: {item[5]}')
 
     except ImportError as e:
         print(f'Erro: {e}')
